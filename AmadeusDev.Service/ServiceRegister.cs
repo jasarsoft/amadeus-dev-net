9372f58f361377afb7f40a81a6fff28e1f1f507f
Edin Jašarević <edinjasar14@gmail.com>
2019-06-22 17:27:28 +0200

    project service

---
+++ AmadeusDev.Service/ServiceRegister.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.Extensions.DependencyInjection;
+//using Jasarsoft.AmadeusDev.Service.IServices;
+//using Jasarsoft.AmadeusDev.Service.Services;
+
+namespace Jasarsoft.AmadeusDev.Service
+{
+    public static class ServiceRegister
+    {
+        public static void AddServices(this IServiceCollection services)
+        {
+
+        }
+    }
+}
