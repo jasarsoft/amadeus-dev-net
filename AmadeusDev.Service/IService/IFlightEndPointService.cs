7bc0b97b83a959755de31cc86562b90bec2198d7
Edin Jašarević <edinjasar14@gmail.com>
2019-06-23 22:58:13 +0200

    service interface

---
+++ AmadeusDev.Service/IService/IFlightEndPointService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Jasarsoft.AmadeusDev.Data.Entities;
+
+namespace Jasarsoft.AmadeusDev.Service.IService
+{
+    public interface IFlightEndPointService
+    {
+    }
+}
