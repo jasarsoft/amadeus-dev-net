7c755463b9f638a0c96d4f9162131ddd610f249b
Edin Jašarević <edinjasar14@gmail.com>
2019-06-23 21:21:40 +0200

    unitofwork

--- AmadeusDev.Repo/RepositoryRegister.cs
+++ AmadeusDev.Repo/RepositoryRegister.cs
@@ -1,13 +1,15 @@
 ﻿using Jasarsoft.AmadeusDev.Repo.IRepositories;
 using Jasarsoft.AmadeusDev.Repo.Repositories;
 using Microsoft.Extensions.DependencyInjection;
-namespace AmadeusDev.Repo
+namespace Jasarsoft.AmadeusDev.Repo
 {
     public static class RepositoryRegister
     {
         public static void AddRepositories(this IServiceCollection service)
         {
+            service.AddTransient<IUnitOfWork, UnitOfWork>();
 
+            
         }
     }
 }
--- AmadeusDev.Repo/UnitOfWork.cs
+++ AmadeusDev.Repo/UnitOfWork.cs
@@ -18,6 +18,66 @@ namespace Jasarsoft.AmadeusDev.Repo
             this.serviceProvider = serviceProvider;
         }
 
+        private IAircraftEquipmentRepository aircraftEquipmentsRepository;
+        public IAircraftEquipmentRepository AircraftEquipments => aircraftEquipmentsRepository ?? (aircraftEquipmentsRepository = serviceProvider.GetService<IAircraftEquipmentRepository>());
+
+        private IDefaultsRepository defaultsRepository;
+        public IDefaultsRepository Defaults => defaultsRepository ?? (defaultsRepository = serviceProvider.GetService<IDefaultsRepository>());
+
+        private IDictionariesRepository dictionariesRepository;
+        public IDictionariesRepository Dictionaries => dictionariesRepository ?? (dictionariesRepository = serviceProvider.GetService<IDictionariesRepository>());
+
+        private IDictionaryEntryRepository dictionaryEntryRepository;
+        public IDictionaryEntryRepository DictionaryEntries => dictionaryEntryRepository ?? (dictionaryEntryRepository = serviceProvider.GetService<IDictionaryEntryRepository>());
+
+        private IFlightEndPointRepository flightEndPointRepository;
+        public IFlightEndPointRepository FlightEndPoints => flightEndPointRepository ?? (flightEndPointRepository = serviceProvider.GetService<IFlightEndPointRepository>());
+
+        private IFlightOfferRepository flightOfferRepository;
+        public IFlightOfferRepository FlightOffer => flightOfferRepository ?? (flightOfferRepository = serviceProvider.GetService<IFlightOfferRepository>());
+
+        private IFlightOffersRepository flightOffersRepository;
+        public IFlightOffersRepository FlightOffers => flightOffersRepository ?? (flightOffersRepository = serviceProvider.GetService<IFlightOffersRepository>());
+
+        private IFlightSegmentRepository flightSegmentRepository;
+        public IFlightSegmentRepository FlightSegments => flightSegmentRepository ?? (flightSegmentRepository = serviceProvider.GetService<IFlightSegmentRepository>());
+
+        private IFlightStopRepository flightStopRepository;
+        public IFlightStopRepository FlightStops => flightStopRepository ?? (flightStopRepository = serviceProvider.GetService<IFlightStopRepository>());
+
+        private IIssueRepository issueRepository;
+        public IIssueRepository Issues => issueRepository ?? (issueRepository = serviceProvider.GetService<IIssueRepository>());
+
+        private IIssueSourceRepository issueSourceRepository;
+        public IIssueSourceRepository IssueSources => issueSourceRepository ?? (issueSourceRepository = serviceProvider.GetService<IIssueSourceRepository>());
+
+        private ILinksRepository linksRepository;
+        public ILinksRepository Links => linksRepository ?? (linksRepository = serviceProvider.GetService<ILinksRepository>());
+
+        private ILocationEntryRepository locationEntryRepository;
+        public ILocationEntryRepository LocationEntries => locationEntryRepository ?? (locationEntryRepository = serviceProvider.GetService<ILocationEntryRepository>());
+
+        private IMetaRepository metaRepository;
+        public IMetaRepository Metas => metaRepository ?? (metaRepository = serviceProvider.GetService<IMetaRepository>());
+
+        private IOfferItemRepository offerItemRepository;
+        public IOfferItemRepository OfferItems => offerItemRepository ?? (offerItemRepository = serviceProvider.GetService<IOfferItemRepository>());
+
+        private IOperatingFlightRepository operatingFlightRepository;
+        public IOperatingFlightRepository OperatingFlights => operatingFlightRepository ?? (operatingFlightRepository = serviceProvider.GetService<IOperatingFlightRepository>());
+
+        private IPriceRepository priceRepository;
+        public IPriceRepository Prices => priceRepository ?? (priceRepository = serviceProvider.GetService<IPriceRepository>());
+
+        private IPricingDetailRepository pricingDetailRepository;
+        public IPricingDetailRepository PricingDetails => pricingDetailRepository ?? (pricingDetailRepository = serviceProvider.GetService<IPricingDetailRepository>());
+
+        private ISegmentRepository segmentRepository;
+        public ISegmentRepository Segments => segmentRepository ?? (segmentRepository = serviceProvider.GetService<ISegmentRepository>());
+
+        private IServiceRepository serviceRepository;
+        public IServiceRepository Services => serviceRepository ?? (serviceRepository = serviceProvider.GetService<IServiceRepository>());
+
         public void Dispose()
         {
             context.Dispose();
