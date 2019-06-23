d1b2897282ca803e267758c088aea6e2a43c13a7
Edin Jašarević <edinjasar14@gmail.com>
2019-06-23 20:59:18 +0200

    repository interface

---
+++ AmadeusDev.Repo/IRepositories/IServiceRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using Jasarsoft.AmadeusDev.Data.Entities;
+
+namespace Jasarsoft.AmadeusDev.Repo.IRepositories
+{
+    public interface IServiceRepository : IRepository<Service, int>
+    {
+    }
+}
--- AmadeusDev.Repo/IUnitOfWork.cs
+++ AmadeusDev.Repo/IUnitOfWork.cs
@@ -7,6 +7,25 @@ namespace Jasarsoft.AmadeusDev.Repo
 {
     public interface IUnitOfWork : IDisposable
     {
-
+        IAircraftEquipmentRepository AircraftEquipments { get; }
+        IDefaultsRepository Defaults { get; }
+        IDictionariesRepository Dictionaries { get; }
+        IDictionaryEntryRepository DictionaryEntries { get; }
+        IFlightEndPointRepository FlightEndPoints { get; }
+        IFlightOfferRepository FlightOffer { get; }
+        IFlightOffersRepository FlightOffers { get; }
+        IFlightSegmentRepository FlightSegments { get; }
+        IFlightStopRepository FlightStops { get; }
+        IIssueRepository Issues { get; }
+        IIssueSourceRepository IssueSources { get; }
+        ILinksRepository Links { get; }
+        ILocationEntryRepository LocationEntries { get; }
+        IMetaRepository Metas { get; }
+        IOfferItemRepository OfferItems { get; }
+        IOperatingFlightRepository OperatingFlights { get; }
+        IPriceRepository Prices { get; }
+        IPricingDetailRepository PricingDetails { get; }
+        ISegmentRepository Segments { get; }
+        IServiceRepository Services { get; }
     }
 }
