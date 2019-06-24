c4f432084eaeeec3b6d9c8e7614312ba300347b9
Edin Jašarević <edinjasar14@gmail.com>
2019-06-24 13:59:43 +0200

    datatable

---
+++ AmadeusDev.Web/Helper/DataTable.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace Jasarsoft.AmadeusDev.Web.Helper
+{
+    public class DataTable<T>
+    {
+        public int Draw { get; set; }
+        public int RecordsTotal { get; set; }
+        public int RecordsFiltered { get; set; }
+        public IEnumerable<T> Data { get; set; }
+
+        public string Error { get; set; }
+    }
+}
