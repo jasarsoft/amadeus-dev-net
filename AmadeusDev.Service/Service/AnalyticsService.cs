7b7a9f2c4fb7a2be33fe7a5b4acd55096fcc9da2
Edin Jašarević <edinjasar14@gmail.com>
2019-06-24 22:59:03 +0200

    services

---
+++ AmadeusDev.Service/Service/AnalyticsService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Jasarsoft.AmadeusDev.Data.Flights;
+using Jasarsoft.AmadeusDev.Repo;
+using Jasarsoft.AmadeusDev.Service.IService;
+
+namespace Jasarsoft.AmadeusDev.Service.Service
+{
+    public class AnalyticsService : IAnalyticsService
+    {
+        private readonly IUnitOfWork unitOfWork;
+
+        public AnalyticsService(IUnitOfWork unitOfWork)
+        {
+            this.unitOfWork = unitOfWork;
+        }
+
+
+    }
+}
---
+++ AmadeusDev.Service/Service/SuccessService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Jasarsoft.AmadeusDev.Data.Flights;
+using Jasarsoft.AmadeusDev.Repo;
+using Jasarsoft.AmadeusDev.Service.IService;
+
+namespace Jasarsoft.AmadeusDev.Service.Service
+{
+    public class SuccessService : ISuccessService
+    {
+        private readonly IUnitOfWork unitOfWork;
+
+        public SuccessService(IUnitOfWork unitOfWork)
+        {
+            this.unitOfWork = unitOfWork;
+        }
+
+
+    }
+}
