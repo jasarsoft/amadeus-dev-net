be1ccef58966fe63466c14fb6a119350b104f6c3
Edin Jašarević <edinjasar14@gmail.com>
2019-06-24 13:53:17 +0200

    area route

--- AmadeusDev.Web/Startup.cs
+++ AmadeusDev.Web/Startup.cs
@@ -74,6 +74,10 @@ namespace Jasarsoft.AmadeusDev.Web
             app.UseMvc(routes =>
             {
                 routes.MapRoute(
+                    name: "area",
+                    template: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
+
+                routes.MapRoute(
                     name: "default",
                     template: "{controller=Home}/{action=Index}/{id?}");
             });
