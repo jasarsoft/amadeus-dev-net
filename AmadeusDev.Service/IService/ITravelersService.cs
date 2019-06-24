69a9c1fe37f35cc9ac461b412036bcea5dfb3741
Edin Jašarević <edinjasar14@gmail.com>
2019-06-24 22:58:11 +0200

    service interface

---
+++ AmadeusDev.Service/IService/ITravelersService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Jasarsoft.AmadeusDev.Data.Airports;
+
+namespace Jasarsoft.AmadeusDev.Service.IService
+{
+    public interface ITravelersService
+    {
+    }
+}
