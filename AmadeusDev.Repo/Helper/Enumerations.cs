5d291c692d23988caad34935e640c55c8dc27a91
Edin Jašarević <edinjasar14@gmail.com>
2019-06-24 21:41:48 +0200

    enumeration

---
+++ AmadeusDev.Repo/Helper/Enumerations.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Jasarsoft.AmadeusDev.Repo.Helper
+{
+    public static class Enumerations
+    {
+        public enum OrderBy
+        {
+            ASC, DESC
+        }
+    }
+}
