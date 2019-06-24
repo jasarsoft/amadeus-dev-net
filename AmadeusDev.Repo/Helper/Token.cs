8271924171957d8d550eb8ec82c3b6320dd4b04b
Edin Jašarević <edinjasar14@gmail.com>
2019-06-25 00:31:11 +0200

    token

---
+++ AmadeusDev.Repo/Helper/Token.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Jasarsoft.AmadeusDev.Repo.Helper
+{
+    public class Token
+    {
+        public string Type { get; set; }
+        public string Username { get; set; }
+        public string Application_name { get; set; }
+        public string Client_id { get; set; }
+        public string Token_type { get; set; }
+        public string Access_token { get; set; }
+        public int Expires_in { get; set; }
+        public string State { get; set; }
+        public string Scope { get; set; }
+    }
+}
