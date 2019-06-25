5bf792ba94540469c3976088a7927ca25152a8cb
Edin Jašarević <edinjasar14@gmail.com>
2019-06-25 20:21:56 +0200

    fixed entity

--- AmadeusDev.Data/Flights/DictionaryEntry.cs
+++ AmadeusDev.Data/Flights/DictionaryEntry.cs
@@ -11,7 +11,6 @@ namespace Jasarsoft.AmadeusDev.Data.Flights
         [Key]
         public int DictionaryEntryId { get; set; }
 
-        [Required]
         public string Code { get; set; }  //code (string)
     }
 }
