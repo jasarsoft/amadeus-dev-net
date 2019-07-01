e9f60e83ad4277880d05e0107dfbfa22aab0f316
Edin Jašarević <edinjasar14@gmail.com>
2019-07-01 15:32:45 +0200

    flight dto fixed

--- AmadeusDev.Repo/DTO/FlightDTO.cs
+++ AmadeusDev.Repo/DTO/FlightDTO.cs
@@ -8,12 +8,12 @@ namespace Jasarsoft.AmadeusDev.Repo.DTO
     {
         public string Departure { get; set; } //polazni aerodrom 
         public string Arrival { get; set; } // odredišni aerodrom 
-        public DateTime DepartureTime { get; set; } // datum polaska
-        public DateTime ArrivalTime { get; set; } // datum povratka
+        public string DepartureTime { get; set; } // datum polaska
+        public string ArrivalTime { get; set; } // datum povratka
         public int DepartureTransfer { get; set; }// broj presjedanja u odlaznom putovanju 
         public int ArrivalTransfer { get; set; } // broj presjedanja u povratnom putovanju 
         public int Passanger { get; set; } // broj putnika 
         public string Currency { get; set; } // valuta
-        public double Price { get; set; } // ukupna cijena 
+        public string Price { get; set; } // ukupna cijena 
     }
 }
