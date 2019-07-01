9f743dad86d0ab2e468bd3d702dad35c36876def
Edin Jašarević <edinjasar14@gmail.com>
2019-07-01 11:44:07 +0200

    flightoffer get by id

--- AmadeusDev.Repo/IRepositories/IFlightOfferRepository.cs
+++ AmadeusDev.Repo/IRepositories/IFlightOfferRepository.cs
@@ -9,5 +9,7 @@ namespace Jasarsoft.AmadeusDev.Repo.IRepositories
     public interface IFlightOfferRepository : IRepository<FlightOffer, int>
     {
         IEnumerable<FlightOffer> GetByFlightId(int flightId);
+        int Insert(FlightOffer model);
+        FlightOffer GetById(string id);
     }
 }
--- AmadeusDev.Repo/Repositories/FlightOfferRepository.cs
+++ AmadeusDev.Repo/Repositories/FlightOfferRepository.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Linq.Expressions;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage;
 using Jasarsoft.AmadeusDev.Data.Flights;
 using Jasarsoft.AmadeusDev.Repo.Context;
 using Jasarsoft.AmadeusDev.Repo.IRepositories;
@@ -14,11 +15,43 @@ namespace Jasarsoft.AmadeusDev.Repo.Repositories
     {
         public FlightOfferRepository(AmadeusDevContext context) : base(context) { }
 
+
         public IEnumerable<FlightOffer> GetByFlightId(int flightId)
         {
             var query = entity.Where(x => x.FlightId == flightId);
 
             return query.ToList();
+        }
+
+        public int Insert(FlightOffer model)
+        {
+            using (IDbContextTransaction transaction = context.Database.BeginTransaction())
+            {
+                try
+                {
+                    var result = GetById(model.Id);
+
+                    if (result == null)
+                    {
+                        context.Add(model);
+                        context.SaveChanges();
+                        transaction.Commit();
+                        return model.FlightOfferId;
+                    }
+
+                    return result.FlightOfferId;
+                }
+                catch (Exception e)
+                {
+                    transaction.Rollback();
+                    throw new DbUpdateException("Insert FlightOffer", e);
+                }
+            }
+        }
+
+        public FlightOffer GetById(string id)
+        {
+            return entity.Where(x => x.Id == id).FirstOrDefault();
         }
     }
 }
